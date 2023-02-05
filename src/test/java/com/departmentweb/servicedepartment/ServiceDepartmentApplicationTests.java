e449913c03c3e681343b9ce5a526c1b6fe82ef80
jitendra shukla <softwaree831@gmail.com>
2023-02-05 22:59:50 +0530

    first commit

---
+++ src/test/java/com/departmentweb/servicedepartment/ServiceDepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.departmentweb.servicedepartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDepartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
